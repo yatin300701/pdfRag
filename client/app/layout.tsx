1ae4bd737edeb318d8ea954deb6d158cee68ab46
Yatin <yatin.choudhary@virtuate.ai>
2025-04-28 23:21:14 +0530

    feat: upload pdf section created

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,12 +1,5 @@
+import { ClerkProvider, SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -36,16 +29,12 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-          {children}
+          <SignedOut>
+            <section className="min-h-screen w-screen flex items-center justify-center">
+              <SignIn />
+            </section>
+          </SignedOut>
+          <SignedIn>{children}</SignedIn>
         </body>
       </html>
     </ClerkProvider>
